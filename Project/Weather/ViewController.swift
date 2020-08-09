80e308770c4f25d437be19887c1832972a5f2741
Александр Шипин <ssanek212@gmail.com>
2020-08-09 12:37:05 +0700

    initial commit

---
+++ Project/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Alex Shipin on 09.08.2020.
+//  Copyright © 2020 Alexander Shipin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
